26c5e43ae9e6c2dcb99116880c4b4a22e39bc053
anandmali <anandm475@gmail.com>
2023-09-14 17:14:52 +0530

    Removed subfolder git

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", function (req, res) {
   res.set("Access-Control-Allow-Origin", "*");
-  res.send("Home Route from the node server");
+  res.send("Simple text response");
 });
 
 const PORT = process.env.PORT || 8080;
